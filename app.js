d698198c44d0a73784f979410dc6594169fd1f27
Nisal-Sashmitha <it20081034@my.sliit.lk>
2023-03-04 09:32:39 +0530

    added write to file funation

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-console.log("hello world");
-
 const fs = require("fs");
 fs.readFile("file.txt", "utf8", function (err, data) {
   if (err) throw err;
   console.log(data);
+});
+
+//Write to file
+fs.writeFile("file.txt", "Hello World!", function (err) {
+  if (err) throw err;
+  console.log("File saved!");
 });
